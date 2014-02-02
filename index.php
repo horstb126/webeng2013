a01433ad48a41ddd658c899575fa049a437ecef4
horst bauchrowitz <horstb@sc-network.de>
2014-02-02 19:16:40 +0100

    leer

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8">
+		<title>Insert title here</title>
+	</head>
+	<body>
+		<?php
+			// Show all information, defaults to INFO_ALL
+			phpinfo();
+			?>
+	</body>
+</html>
